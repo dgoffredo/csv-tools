9800a1c157964109111ac72b600874e211389c52
David Goffredo <dmgoffredo@gmail.com>
2021-10-15 02:20:16 -0400

    use cons cells instead of arrays

--- expand_commas/expand_commas.py
+++ expand_commas/expand_commas.py
@@ -7,19 +7,25 @@ import sys
 
 def expand_row(row, column_index=0):
     if column_index == len(row):
-        yield []
+        yield None
         return
         
     for value in row[column_index]:
         suffixes = expand_row(row, column_index + 1)
         for suffix in suffixes:
-            yield [value.strip(), *suffix]
-            
+            yield (value.strip(), suffix)
+
+
+def chain_iter(chain):
+    while chain is not None:
+        value, chain = chain
+        yield value
+
 
 if __name__ == '__main__':
     reader = csv.reader(sys.stdin)
     writer = csv.writer(sys.stdout)
     for row in reader:
         parsed_row = [value.split(',') for value in row]
-        for output_row in expand_row(parsed_row):
-            writer.writerow(output_row)
+        for output_chain in expand_row(parsed_row):
+            writer.writerow(chain_iter(output_chain))
